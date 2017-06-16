693cbd7fa4f0d6344cddc2a780382eabad4ff540
Zhi Wei Lim <eugene.lim@yale.edu>
2017-06-16 06:51:49 +0000

    Minimum viable product

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# neighborhood-map
-Neighborhood map project for Udacity Full Stack Web Developer Nanodegree.
+# New Haven Pizza Map
+Neighborhood map project for Udacity Full Stack Web Developer Nanodegree. Shows pizza places in New Haven with live search and additional Zomato information.
+
+## Installation
+1. Clone the project and `cd` into it.
+2. Install the dependencies via bower `bower install bootstrap#v4.0.0-alpha.6` `bower install knockout`.
+3. Generate a [Zomato API key](https://developers.zomato.com/api) and add it to `user-key` in `js/app.js`.
+4. Generate a [Google Maps JavaScript API key](https://developers.google.com/maps/documentation/javascript/) add add it to `<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&v=3"></script>` in `index.html`.
+5. Open `index.html` and enjoy!
+
+## Credits
+* The dashboard was built off Bootstrap 4's [Dashboard example](https://v4-alpha.getbootstrap.com/examples/).
+* Pizza icon from [IconArchive](http://www.iconarchive.com/show/swarm-icons-by-sonya/Pizza-icon.html).
+*
+## Possible improvements
+* Better presentation of Zomato data
+* Photos
+* Automatically toggle navbar on click
+* Filter by price and rating
+* Filter by location
---
+++ js/app.js
@@ -0,0 +1,832 @@
+var restaurants = [{
+  "lat": 41.306823,
+  "lng": -72.927977,
+  "id": 17711589,
+  "name": "Kitchen ZINC",
+  "address": "966 Chapel St [behind ZINC] 06510"
+}, {
+  "lat": 41.3064469,
+  "lng": -72.929538,
+  "id": 17711801,
+  "name": "Bella Haven Pizza Restaurant",
+  "address": "240 College St 06510"
+}, {
+  "lat": 41.3309,
+  "lng": -72.8674,
+  "id": 17862883,
+  "name": "Quinnipiac Pizza & Deli",
+  "address": "1653 Quinnipiac Avenue 06513"
+}, {
+  "lat": 41.3062,
+  "lng": -72.9276,
+  "id": 17862872,
+  "name": "PizzaZiki Pizzeria",
+  "address": "170 Temple St New Haven 06510"
+}, {
+  "lat": 41.3103,
+  "lng": -72.9258,
+  "id": 17710161,
+  "name": "Wall Street Pizza & Restaurant",
+  "address": "90 Wall St 06511"
+}, {
+  "lat": 41.3109,
+  "lng": -72.93,
+  "id": 17709895,
+  "name": "A-1 Pizza On Broadway",
+  "address": "21 Broadway 06511"
+}, {
+  "lat": 41.3113,
+  "lng": -72.9297,
+  "id": 17710327,
+  "name": "Yorkside Pizza And Restaurant",
+  "address": "288 York St 06511"
+}, {
+  "lat": 41.3052084,
+  "lng": -72.9263915,
+  "id": 17712210,
+  "name": "Red Tomado Wood Fired Pizza",
+  "address": "72 Church St 06510"
+}, {
+  "lat": 41.3067,
+  "lng": -72.9318,
+  "id": 17709919,
+  "name": "Bar",
+  "address": "254 Crown St 06511"
+}, {
+  "lat": 41.3068,
+  "lng": -72.9319,
+  "id": 17709900,
+  "name": "Aladdin Crown Pizza",
+  "address": "260 Crown St 06511"
+}, {
+  "lat": 41.3058,
+  "lng": -72.9243,
+  "id": 17710151,
+  "name": "Mediterranea Restaurant",
+  "address": "140 Orange St 06510"
+}, {
+  "lat": 41.3087,
+  "lng": -72.9334,
+  "id": 17710020,
+  "name": "EST EST EST Pizza & Restaurant",
+  "address": "1176 Chapel St 06511"
+}, {
+  "lat": 41.311196,
+  "lng": -72.932168,
+  "id": 17712200,
+  "name": "Tarry Lodge Enoteca and Pizzeria",
+  "address": "278 Park St 06511"
+}, {
+  "lat": 41.3106536865,
+  "lng": -72.9224684089,
+  "id": 17712083,
+  "name": "Costa Pizza",
+  "address": "9 Whitney Avenue, New Haven 06510"
+}, {
+  "lat": 41.3056,
+  "lng": -72.9334,
+  "id": 17710189,
+  "name": "Pizza Corner",
+  "address": "45 York St 06511"
+}, {
+  "lat": 41.304,
+  "lng": -72.9245,
+  "id": 17710142,
+  "name": "Marco Polo Pizza & Italian",
+  "address": "55 Crown St, New Haven 06510"
+}, {
+  "lat": 41.3111,
+  "lng": -72.9222,
+  "id": 17710295,
+  "name": "Town Pizza",
+  "address": "25 Whitney Ave 06510"
+}, {
+  "lat": 41.3119,
+  "lng": -72.933,
+  "id": 17709902,
+  "name": "Alpha Delta Pizza",
+  "address": "371 Elm St 06511"
+}, {
+  "lat": 41.3105,
+  "lng": -72.9345,
+  "id": 17710192,
+  "name": "Pizza House",
+  "address": "89 Howe St 06511"
+}, {
+  "lat": 41.3121,
+  "lng": -72.9336,
+  "id": 17710323,
+  "name": "Woodburning",
+  "address": "122 Howe St 06511"
+}, {
+  "lat": 41.3121,
+  "lng": -72.9336,
+  "id": 17709937,
+  "name": "Brick Oven Pizza",
+  "address": "122 Howe St 06511"
+}, {
+  "lat": 41.3124,
+  "lng": -72.9222,
+  "id": 17709980,
+  "name": "Clarks' Pizza & Restaurant",
+  "address": "68 Whitney Ave 06510"
+}, {
+  "lat": 41.304709,
+  "lng": -72.934326,
+  "id": 17712181,
+  "name": "Lavero Pizza",
+  "address": "45 York St 06510"
+}, {
+  "lat": 41.3126,
+  "lng": -72.9218,
+  "id": 17710150,
+  "name": "Mediterranea",
+  "address": "77 Whitney Ave 06510"
+}, {
+  "lat": 41.313,
+  "lng": -72.9335,
+  "id": 17710190,
+  "name": "Pizza Empire",
+  "address": "16 Whalley Ave 06511"
+}, {
+  "lat": 41.3073,
+  "lng": -72.9372,
+  "id": 17710191,
+  "name": "Pizza Haven",
+  "address": "34 Howe St 06511"
+}, {
+  "lat": 41.3049,
+  "lng": -72.9373,
+  "id": 17710054,
+  "name": "Gennaro's Apizza",
+  "address": "888 Howard Ave 06519"
+}, {
+  "lat": 41.303,
+  "lng": -72.9199,
+  "id": 17710226,
+  "name": "Sally's Apizza",
+  "address": "237 Wooster St 06511"
+}, {
+  "lat": 41.3148,
+  "lng": -72.9366,
+  "id": 17710184,
+  "name": "Papa John's Pizza",
+  "address": "125 Whalley Ave 06511"
+}, {
+  "lat": 41.3029,
+  "lng": -72.9173,
+  "id": 17710252,
+  "name": "The Spot, Frank Pepe Pizzeria",
+  "address": "163 Wooster St 06511"
+}, {
+  "lat": 41.303,
+  "lng": -72.9171,
+  "id": 17710050,
+  "name": "Frank Pepe Pizzeria Napoletana",
+  "address": "157 Wooster St 06511"
+}, {
+  "lat": 41.315016,
+  "lng": -72.917133,
+  "id": 17712028,
+  "name": "Cafe Romeo",
+  "address": "534 Orange St 06511"
+}, {
+  "lat": 41.3029,
+  "lng": -72.9155,
+  "id": 17709897,
+  "name": "Abate Restaurant",
+  "address": "129 Wooster St 06511"
+}, {
+  "lat": 41.300522,
+  "lng": -72.938125,
+  "id": 17712026,
+  "name": "Daddy's 1 Pizza",
+  "address": "560 Congress Ave 06519"
+}, {
+  "lat": 41.3184,
+  "lng": -72.9339,
+  "id": 17710005,
+  "name": "Downtown Pizza",
+  "address": "182 Dixwell Ave Ste 1 06511"
+}, {
+  "lat": 41.311,
+  "lng": -72.9422,
+  "id": 17709967,
+  "name": "Chapel Street Pizza",
+  "address": "1409 Chapel St 06511"
+}, {
+  "lat": 41.308163,
+  "lng": -72.9123725,
+  "id": 17803420,
+  "name": "Pizza Plus",
+  "address": "157 Hamilton St 06511"
+}, {
+  "lat": 41.3133993,
+  "lng": -72.9133989,
+  "id": 17711758,
+  "name": "Da Legna: Wood Fired Pizza",
+  "address": "858 State Street 06511"
+}, {
+  "lat": 41.316605,
+  "lng": -72.9156429,
+  "id": 17711757,
+  "name": "Nica's Market",
+  "address": "603 Orange Street 06511"
+}, {
+  "lat": 41.314,
+  "lng": -72.9128,
+  "id": 17710154,
+  "name": "Modern Apizza",
+  "address": "874 State Street 06511"
+}, {
+  "lat": 41.3172,
+  "lng": -72.9443,
+  "id": 17710317,
+  "name": "Whalley Pizza",
+  "address": "350 Whalley Ave 06511"
+}, {
+  "lat": 41.3102,
+  "lng": -72.9483,
+  "id": 17709894,
+  "name": "New Haven Pizza",
+  "address": "798 George St 06511"
+}, {
+  "lat": 41.3175,
+  "lng": -72.9449,
+  "id": 17710004,
+  "name": "Domino's",
+  "address": "357 Whalley Ave 06511"
+}, {
+  "lat": 41.3161,
+  "lng": -72.9095,
+  "id": 17710195,
+  "name": "Planet Apizza",
+  "address": "996 State St 06511"
+}, {
+  "lat": 41.2941,
+  "lng": -72.938496,
+  "id": 17711897,
+  "name": "Apizza Grande",
+  "address": "630 Washington Ave 06519"
+}, {
+  "lat": 41.3179,
+  "lng": -72.9456,
+  "id": 17710299,
+  "name": "Triple A Pizza",
+  "address": "383 Whalley Ave 06511"
+}, {
+  "lat": 41.291,
+  "lng": -72.9333,
+  "id": 17710115,
+  "name": "Kimberly Pizza",
+  "address": "34 Kimberly Ave 06519"
+}, {
+  "lat": 41.3187617,
+  "lng": -72.9483601,
+  "id": 17712101,
+  "name": "Little Caesars Pizza",
+  "address": "454 Whalley Ave 06511"
+}, {
+  "lat": 41.3209,
+  "lng": -72.9097,
+  "id": 17710193,
+  "name": "Pizza Plus Incorporated",
+  "address": "565 Ferry St 06513"
+}, {
+  "lat": 41.3104,
+  "lng": -72.9528,
+  "id": 17709938,
+  "name": "Broadway Pizza",
+  "address": "185 Derby Ave 06511"
+}, {
+  "lat": 41.3084,
+  "lng": -72.9026,
+  "id": 17709915,
+  "name": "Avellino's Apizza Restaurant",
+  "address": "384 Grand Ave 06513"
+}, {
+  "lat": 41.3211,
+  "lng": -72.9091,
+  "id": 17710022,
+  "name": "East Rock Pizza",
+  "address": "163 Foster St 06511"
+}, {
+  "lat": 41.3191,
+  "lng": -72.9494,
+  "id": 17709991,
+  "name": "Crown Fried Chicken",
+  "address": "474 Whalley Ave 06511"
+}, {
+  "lat": 41.32179,
+  "lng": -72.90871,
+  "id": 17712099,
+  "name": "Angelo's Pizza",
+  "address": "163 Foster St 06511"
+}, {
+  "lat": 41.286968973,
+  "lng": -72.9276558624,
+  "id": 18228483,
+  "name": "Blaze Pizza",
+  "address": "40 Sargent Drive, New Haven 06511"
+}, {
+  "lat": 41.2855,
+  "lng": -72.9365,
+  "id": 17710296,
+  "name": "Townhouse Pizza",
+  "address": "246 Kimberly Ave 06519"
+}, {
+  "lat": 41.3088,
+  "lng": -72.8957,
+  "id": 17710304,
+  "name": "USA Pizza",
+  "address": "216 Grand Ave 06513"
+}, {
+  "lat": 41.3152998,
+  "lng": -72.8967891,
+  "id": 17803388,
+  "name": "Liberty Pizza",
+  "address": "565 Ferry St 06513"
+}, {
+  "lat": 41.2959,
+  "lng": -72.9573,
+  "id": 17711123,
+  "name": "Cappetta Italian Import",
+  "address": "188 Boston Post Rd 06516"
+}, {
+  "lat": 41.304115,
+  "lng": -72.894485,
+  "id": 17711675,
+  "name": "Pizza By Romano's",
+  "address": "152 Ferry St. 06513"
+}, {
+  "lat": 41.3042,
+  "lng": -72.8943,
+  "id": 17710188,
+  "name": "Pizza By Romano's",
+  "address": "152 Ferry St 06513"
+}, {
+  "lat": 41.3094,
+  "lng": -72.8918,
+  "id": 17710065,
+  "name": "Grand Apizza",
+  "address": "111 Grand Ave 06513"
+}, {
+  "lat": 41.3238,
+  "lng": -72.8961,
+  "id": 17710098,
+  "name": "Jack's Bar & Grill LLC",
+  "address": "1505 State St 06511"
+}, {
+  "lat": 41.2952,
+  "lng": -72.8933,
+  "id": 17710312,
+  "name": "Wagon Wheel Cafe",
+  "address": "20 Farren Ave 06513"
+}, {
+  "lat": 41.3286,
+  "lng": -72.9565,
+  "id": 17710158,
+  "name": "More Than Pizza Incorporated",
+  "address": "283 Blake St 06515"
+}, {
+  "lat": 41.32853,
+  "lng": -72.956694,
+  "id": 17711624,
+  "name": "Blake Street Pizza",
+  "address": "410 Blake Street 06515"
+}, {
+  "lat": 41.328773,
+  "lng": -72.9565,
+  "id": 17711923,
+  "name": "Pizza Heaven II",
+  "address": "410 Blake St # 1 06515"
+}, {
+  "lat": 41.3264,
+  "lng": -72.9596,
+  "id": 17712126,
+  "name": "Pizza Hut",
+  "address": "864-A Whalley Ave 06515"
+}, {
+  "lat": 41.290339,
+  "lng": -72.895465,
+  "id": 17803342,
+  "name": "Frisco's Pizza",
+  "address": "383 Forbes Ave 06512"
+}, {
+  "lat": 41.327,
+  "lng": -72.9598,
+  "id": 17710316,
+  "name": "Westville Pizza",
+  "address": "883 Whalley Ave 06515"
+}, {
+  "lat": 41.3079,
+  "lng": -72.9692,
+  "id": 17711189,
+  "name": "Michaelangelo Pizza & Subs Restaurant",
+  "address": "425 Derby Ave 06516"
+}, {
+  "lat": 41.3085,
+  "lng": -72.8863,
+  "id": 17710330,
+  "name": "Ziggy's Pizza Restaurant",
+  "address": "36 E Grand Ave 06513"
+}, {
+  "lat": 41.33846,
+  "lng": -72.94079,
+  "id": 17711907,
+  "name": "Campus Pizza",
+  "address": "141 Arch St 06514"
+}, {
+  "lat": 41.2886,
+  "lng": -72.8935,
+  "id": 17710217,
+  "name": "Roma Pizza LLC",
+  "address": "43 Main Street Anx 06512"
+}, {
+  "lat": 41.278,
+  "lng": -72.9446,
+  "id": 17711222,
+  "name": "Seaside Pizza",
+  "address": "126 Elm St 06516"
+}, {
+  "lat": 41.3426,
+  "lng": -72.935,
+  "id": 17709697,
+  "name": "Pianca Pizza",
+  "address": "1072 Dixwell Ave 06514"
+}, {
+  "lat": 41.2733,
+  "lng": -72.9376,
+  "id": 17711187,
+  "name": "Marinos Deli and Pizza",
+  "address": "196 1st Ave 06516"
+}, {
+  "lat": 41.2777,
+  "lng": -72.9536,
+  "id": 17711206,
+  "name": "Pizza Works",
+  "address": "705 Campbell Ave 06516"
+}, {
+  "lat": 41.3289,
+  "lng": -72.9688,
+  "id": 17709997,
+  "name": "Dayton Street Apizza",
+  "address": "60 Dayton St 06515"
+}, {
+  "lat": 41.2735,
+  "lng": -72.9446,
+  "id": 17711258,
+  "name": "Zuppardi's Apizza",
+  "address": "179 Union Ave 06516"
+}, {
+  "lat": 41.274844,
+  "lng": -72.951959,
+  "id": 17712170,
+  "name": "Usa 1 Pizza",
+  "address": "616 Campbell Ave 06516"
+}, {
+  "lat": 41.3463,
+  "lng": -72.934,
+  "id": 17709699,
+  "name": "Pizza Choice",
+  "address": "1216 Dixwell Ave 06514"
+}, {
+  "lat": 41.2711,
+  "lng": -72.941,
+  "id": 17711207,
+  "name": "Pizza di Roma",
+  "address": "28 Brown St 06516"
+}, {
+  "lat": 41.346792,
+  "lng": -72.9336057,
+  "id": 17712080,
+  "name": "Little Caesars Pizza",
+  "address": "1225 Dixwell Ave 06514"
+}, {
+  "lat": 41.2730377167,
+  "lng": -72.9503338784,
+  "id": 17712030,
+  "name": "Us Empire Pizza",
+  "address": "543 Campbell Ave 06516"
+}, {
+  "lat": 41.331,
+  "lng": -72.9725,
+  "id": 17710036,
+  "name": "Ernie's Pizzeria",
+  "address": "1279 Whalley Ave 06515"
+}, {
+  "lat": 41.33121,
+  "lng": -72.973145,
+  "id": 17711924,
+  "name": "Clemente's Pizza & Grill",
+  "address": "1302 Whalley Ave 06515"
+}, {
+  "lat": 41.3502,
+  "lng": -72.932,
+  "id": 17709689,
+  "name": "Paesano's Pizza",
+  "address": "1370 Dixwell Ave 06514"
+}, {
+  "lat": 41.3506,
+  "lng": -72.9318,
+  "id": 17709720,
+  "name": "Star Pizza Restaurant",
+  "address": "1380 Dixwell Ave 06514"
+}, {
+  "lat": 41.3331,
+  "lng": -72.9759,
+  "id": 17710251,
+  "name": "Spooner Restaurant & Pizza",
+  "address": "1400 Whalley Ave 06515"
+}, {
+  "lat": 41.274,
+  "lng": -72.9645,
+  "id": 17711205,
+  "name": "Pizza Palace of West Haven",
+  "address": "215 Saw Mill Rd 06516"
+}, {
+  "lat": 41.2712,
+  "lng": -72.9597,
+  "id": 17711108,
+  "name": "Andy's Pizza",
+  "address": "38 Saw Mill Rd Ste 12 06516"
+}, {
+  "lat": 41.27,
+  "lng": -72.9585,
+  "id": 17711215,
+  "name": "Ronnie Apizza",
+  "address": "549 Main St 06516"
+}, {
+  "lat": 41.2742,
+  "lng": -72.9665,
+  "id": 17711204,
+  "name": "Pizza Hut",
+  "address": "249 Saw Mill Rd 06516"
+}, {
+  "lat": 41.3535,
+  "lng": -72.9302,
+  "id": 17709679,
+  "name": "Legend Pizza",
+  "address": "1500 Dixwell Ave 06514"
+}, {
+  "lat": 41.2828,
+  "lng": -72.8778,
+  "id": 17709551,
+  "name": "Minervini's Pizzeria",
+  "address": "457 Main St 06512"
+}, {
+  "lat": 41.2823,
+  "lng": -72.8778,
+  "id": 17709528,
+  "name": "De Palma's Apizza",
+  "address": "440 Main Street, East Haven 06512"
+}, {
+  "lat": 41.2643,
+  "lng": -72.947,
+  "id": 17711126,
+  "name": "Casabianca Pizza",
+  "address": "217 Campbell Ave 06516"
+}, {
+  "lat": 41.2641,
+  "lng": -72.9469,
+  "id": 17711197,
+  "name": "Only the Best Pizza",
+  "address": "209 Campbell Ave 06516"
+}, {
+  "lat": 41.2814,
+  "lng": -72.877,
+  "id": 17709567,
+  "name": "Tolli's Apizza",
+  "address": "410 Main St 06512"
+}]
+
+function RestaurantsViewModel() {
+  var self = this;
+  var init = false;
+
+  self.query = ko.observable('');
+
+  self.restaurantList = ko.computed(function() {
+    var restaurantList = [];
+    for (var i = 0; i < restaurants.length; i++) {
+      if (restaurants[i].name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
+        restaurantList.push(restaurants[i]);
+      }
+    }
+    return restaurantList;
+  }, this);
+
+  self.addMarkers = function() {
+    addMarkers(self.restaurantList(), map);
+    if (init) {
+      linkMarkers(markers);
+    }
+    else {
+      init = true;
+    }
+  }
+}
+
+function initMap() {
+  var styles = [{
+    "featureType": "landscape",
+    "stylers": [{
+      "hue": "#FFBB00"
+    }, {
+      "saturation": 43.400000000000006
+    }, {
+      "lightness": 37.599999999999994
+    }, {
+      "gamma": 1
+    }]
+  }, {
+    "featureType": "road.highway",
+    "stylers": [{
+      "hue": "#FFC200"
+    }, {
+      "saturation": -61.8
+    }, {
+      "lightness": 45.599999999999994
+    }, {
+      "gamma": 1
+    }]
+  }, {
+    "featureType": "road.arterial",
+    "stylers": [{
+      "hue": "#FF0300"
+    }, {
+      "saturation": -100
+    }, {
+      "lightness": 51.19999999999999
+    }, {
+      "gamma": 1
+    }]
+  }, {
+    "featureType": "road.local",
+    "stylers": [{
+      "hue": "#FF0300"
+    }, {
+      "saturation": -100
+    }, {
+      "lightness": 52
+    }, {
+      "gamma": 1
+    }]
+  }, {
+    "featureType": "water",
+    "stylers": [{
+      "hue": "#0078FF"
+    }, {
+      "saturation": -13.200000000000003
+    }, {
+      "lightness": 2.4000000000000057
+    }, {
+      "gamma": 1
+    }]
+  }, {
+    "featureType": "poi",
+    "stylers": [{
+      "hue": "#00FF6A"
+    }, {
+      "saturation": -1.0989010989011234
+    }, {
+      "lightness": 11.200000000000017
+    }, {
+      "gamma": 1
+    }]
+  }];
+
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 41.3083,
+      lng: -72.9279
+    },
+    zoom: 14,
+    styles: styles,
+    mapTypeControl: false
+  });
+
+  return map;
+}
+
+function addMarkers(list, map) {
+  // Clear existing markers
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+
+  for (var i = 0; i < list.length; i++) {
+    // Create a marker per location, and put into markers array.
+    var marker = new google.maps.Marker({
+      position: {
+        lat: list[i].lat,
+        lng: list[i].lng
+      },
+      title: list[i].name,
+      map: map,
+      icon: '/images/pizza_icon.png',
+      animation: google.maps.Animation.DROP,
+      id: list[i].id
+    });
+    // Push the marker to our array of markers.
+    markers.push(marker);
+
+    // Change icon size on mouseover
+    marker.addListener('mouseover', function() {
+      this.setIcon('/images/pizza_icon_large.png');
+    });
+    marker.addListener('mouseout', function() {
+      this.setIcon('/images/pizza_icon.png');
+    });
+
+    // Another closure! Create an onclick event to open the large infowindow at each marker.
+    marker.addListener('click', function(marker) {
+      return function() {
+        map.panTo(marker.position);
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+          marker.setAnimation(null);
+        }, 750);
+        populateInfoWindow(marker, largeInfowindow);
+      }
+    }(marker));
+  }
+}
+
+function linkMarkers(markers) {
+  // Wow, a closure! Adds event listener to open infowindow for link clicks.
+  for (var i = 0; i < markers.length; i++) {
+    var link = document.getElementById(markers[i].id);
+    var marker = markers[i];
+    link.addEventListener('click', function(marker) {
+      return function() {
+        map.panTo(marker.position);
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+          marker.setAnimation(null);
+        }, 750);
+        populateInfoWindow(marker, largeInfowindow);
+      }
+    }(marker));
+  }
+}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    // Clear the infowindow content to give the streetview time to load.
+    infowindow.setContent('');
+    infowindow.marker = marker;
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
+    });
+
+    // Sent AJAX request to Zomato, failing gracefully if error found.
+    $.ajax({
+      url: "https://developers.zomato.com/api/v2.1/restaurant",
+      type: "GET",
+      data: {
+        res_id: marker.id
+      },
+      headers: {
+        "user-key": "a372a4ad119e5e91d5ae63c54225f12d"
+      },
+      dataType: "json",
+
+      success: function(data) {
+        var rating = data.user_rating.rating_text
+        if (data.average_cost_for_two != 0) {
+          var cost = "$" + data.average_cost_for_two.toString();
+        }
+        else {
+          cost = "Not found";
+        }
+        infowindow.setContent('<h5>' + marker.title + '</h5>' +
+          '<li>Rating: ' + rating + '</li><li>Average cost for two: ' + cost + '</li>');
+
+      },
+      error: function(err) {
+        console.log(err);
+        infowindow.setContent('<h5>' + marker.title + '</h5>' + 'No data found.');
+      }
+    });
+
+    infowindow.open(map, marker);
+  }
+}
+
+// Global variables
+var map = initMap();
+var markers = []
+var largeInfowindow = new google.maps.InfoWindow();
+var RestaurantViewModel = new
+
+// Initialize
+ko.applyBindings(RestaurantViewModel);
+linkMarkers(markers);
